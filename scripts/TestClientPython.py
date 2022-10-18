1f8d4a8eafb68df18d7f50e0fd04b8392ceb8262
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-10-18 09:46:32 +0200

    Reformat with black

--- scripts/TestClientPython.py
+++ scripts/TestClientPython.py
@@ -8,7 +8,7 @@ import time
 
 # Find code directory relative to our directory
 THIS_DIR = dirname(__file__)
-CODE_DIR = abspath(join(THIS_DIR, '..', 'vicon_dssdk'))
+CODE_DIR = abspath(join(THIS_DIR, "..", "vicon_dssdk"))
 sys.path.append(CODE_DIR)
 from vicon_dssdk import ViconDataStream
 
@@ -18,39 +18,46 @@ import sys
 
 # parse arguments
 import argparse
-parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument('host', nargs='?', help="Host name, in the format of server:port", default = "localhost:801")
-args = parser.parse_args()
 
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument(
+    "host",
+    nargs="?",
+    help="Host name, in the format of server:port",
+    default="localhost:801",
+)
+args = parser.parse_args()
 
 
 # handle ctrl+c to disconnect
 def signal_handler(sig, frame):
-    print('You pressed Ctrl+C! -> closing ')
+    print("You pressed Ctrl+C! -> closing ")
     client.Disconnect()
-    print('zmq closed? ' + str(socket.closed))
+    print("zmq closed? " + str(socket.closed))
     context.destroy()
-    print('zmq closed? ' + str(socket.closed))
-    print('bye...')
+    print("zmq closed? " + str(socket.closed))
+    print("bye...")
     sys.exit(0)
+
+
 signal.signal(signal.SIGINT, signal_handler)
 
 # start vicon client
 client = ViconDataStream.Client()
 j = {}
 tried = 0
-while tried<10:
+while tried < 10:
     try:
-        print('try num: ' + str(tried))
-        client.Connect( args.host )
+        print("try num: " + str(tried))
+        client.Connect(args.host)
 
         # Check the version
         # print( 'Version', client.GetVersion() )
 
         # Check setting the buffer size works
-        client.SetBufferSize( 1 )
+        client.SetBufferSize(1)
 
-        #Enable all the data types
+        # Enable all the data types
         client.EnableSegmentData()
         client.EnableMarkerData()
         client.EnableUnlabeledMarkerData()
@@ -69,18 +76,26 @@ while tried<10:
         # establish zmq connection
         context = zmq.Context()
         socket = context.socket(zmq.PUB)
-        socket.bind("tcp://10.42.2.29:5555")#
+        socket.bind("tcp://10.42.2.29:5555")  #
 
-        num_frame=0
+        num_frame = 0
         start_time = time.time_ns()
         while True:
             frame_time = time.time_ns()
-            on_time = (time.time_ns()-start_time)/1000000000
-            
+            on_time = (time.time_ns() - start_time) / 1000000000
+
             HasFrame = False
-            num_frame=num_frame+1
-            if num_frame%150 ==0:
-                msg = 'frame # ' + str(num_frame) + ' in ' + str(on_time) + ' s -> ' + str(num_frame/on_time) + ' frames/s'
+            num_frame = num_frame + 1
+            if num_frame % 150 == 0:
+                msg = (
+                    "frame # "
+                    + str(num_frame)
+                    + " in "
+                    + str(on_time)
+                    + " s -> "
+                    + str(num_frame / on_time)
+                    + " frames/s"
+                )
                 print(msg)
 
             while not HasFrame:
@@ -89,46 +104,60 @@ while tried<10:
                     HasFrame = True
                 except ViconDataStream.DataStreamException as e:
                     client.GetFrame()
-            
+
             # Try setting the different stream modes
-            client.SetStreamMode( ViconDataStream.Client.StreamMode.EClientPull )
+            client.SetStreamMode(ViconDataStream.Client.StreamMode.EClientPull)
             # print( 'Get Frame Pull', client.GetFrame(), client.GetFrameNumber() )
 
-            client.SetStreamMode( ViconDataStream.Client.StreamMode.EClientPullPreFetch )
+            client.SetStreamMode(ViconDataStream.Client.StreamMode.EClientPullPreFetch)
             # print( 'Get Frame PreFetch', client.GetFrame(), client.GetFrameNumber() )
 
-            client.SetStreamMode( ViconDataStream.Client.StreamMode.EServerPush )
+            client.SetStreamMode(ViconDataStream.Client.StreamMode.EServerPush)
             # print( 'Get Frame Push', client.GetFrame(), client.GetFrameNumber() )
-            j["frame_number"] =client.GetFrameNumber()
-            j["my_frame_number"]=num_frame
-            j["on_time"] = (time.time_ns()-start_time)/1000000000
+            j["frame_number"] = client.GetFrameNumber()
+            j["my_frame_number"] = num_frame
+            j["on_time"] = (time.time_ns() - start_time) / 1000000000
 
             # print( 'Frame Rate', client.GetFrameRate() )
-            j["frame_rate"] =  client.GetFrameRate()
+            j["frame_rate"] = client.GetFrameRate()
 
-            hours, minutes, seconds, frames, subframe, fieldFlag, standard, subFramesPerFrame, userBits = client.GetTimecode()
-            # print( ('Timecode:', hours, 'hours', minutes, 'minutes', seconds, 'seconds', frames, 
-            #     'frames', subframe, 'sub frame', fieldFlag, 'field flag', 
+            (
+                hours,
+                minutes,
+                seconds,
+                frames,
+                subframe,
+                fieldFlag,
+                standard,
+                subFramesPerFrame,
+                userBits,
+            ) = client.GetTimecode()
+            # print( ('Timecode:', hours, 'hours', minutes, 'minutes', seconds, 'seconds', frames,
+            #     'frames', subframe, 'sub frame', fieldFlag, 'field flag',
             #     standard, 'standard', subFramesPerFrame, 'sub frames per frame', userBits, 'user bits') )
             j["time_stamp"] = time.time_ns()
 
             # print( 'Total Latency', client.GetLatencyTotal() )
-            j["latency"]=client.GetLatencyTotal()
+            j["latency"] = client.GetLatencyTotal()
 
             # print( 'Latency Samples' )
             # for sampleName, sampleValue in client.GetLatencySamples().items():
-                # print( sampleName, sampleValue )
+            # print( sampleName, sampleValue )
 
             # print( 'Frame Rates' )
             # for frameRateName, frameRateValue in client.GetFrameRates().items():
-                # print( frameRateName, frameRateValue )
+            # print( frameRateName, frameRateValue )
 
             # try:
             #     client.SetApexDeviceFeedback( 'BogusDevice', True )
             # except ViconDataStream.DataStreamException as e:
             #     print( 'No Apex Devices connected' )
 
-            client.SetAxisMapping( ViconDataStream.Client.AxisMapping.EForward, ViconDataStream.Client.AxisMapping.ELeft, ViconDataStream.Client.AxisMapping.EUp )
+            client.SetAxisMapping(
+                ViconDataStream.Client.AxisMapping.EForward,
+                ViconDataStream.Client.AxisMapping.ELeft,
+                ViconDataStream.Client.AxisMapping.EUp,
+            )
             xAxis, yAxis, zAxis = client.GetAxisMapping()
             # print( 'X Axis', xAxis, 'Y Axis', yAxis, 'Z Axis', zAxis )
 
@@ -144,72 +173,87 @@ while tried<10:
             # except ViconDataStream.DataStreamException as e:
             #     print( 'Failed to configure wireless', e )
 
-
             subjectNames = client.GetSubjectNames()
-            j['subjectNames'] = subjectNames
-            j['num_subjects'] = len(subjectNames)
-            sbj_idx=0
+            j["subjectNames"] = subjectNames
+            j["num_subjects"] = len(subjectNames)
+            sbj_idx = 0
             for subjectName in subjectNames:
                 # print( subjectName )
-                sbj_idx_str = 'subject_' + str(sbj_idx)
-                sbj_idx=sbj_idx+1
-                
-                segmentNames = client.GetSegmentNames( subjectName )
+                sbj_idx_str = "subject_" + str(sbj_idx)
+                sbj_idx = sbj_idx + 1
+
+                segmentNames = client.GetSegmentNames(subjectName)
                 for segmentName in segmentNames:
-                    segmentChildren = client.GetSegmentChildren( subjectName, segmentName )
+                    segmentChildren = client.GetSegmentChildren(
+                        subjectName, segmentName
+                    )
                     # for child in segmentChildren:
-                        # try:
-                            # print( child, 'has parent', client.GetSegmentParentName( subjectName, segmentName ) )
-                        # except ViconDataStream.DataStreamException as e:
-                            # print( 'Error getting parent segment', e )
+                    # try:
+                    # print( child, 'has parent', client.GetSegmentParentName( subjectName, segmentName ) )
+                    # except ViconDataStream.DataStreamException as e:
+                    # print( 'Error getting parent segment', e )
                     # print( segmentName, 'has static translation', client.GetSegmentStaticTranslation( subjectName, segmentName ) )
-                    # print( segmentName, 'has static rotation( helical )', client.GetSegmentStaticRotationHelical( subjectName, segmentName ) )               
-                    # print( segmentName, 'has static rotation( EulerXYZ )', client.GetSegmentStaticRotationEulerXYZ( subjectName, segmentName ) )              
-                    # print( segmentName, 'has static rotation( Quaternion )', client.GetSegmentStaticRotationQuaternion( subjectName, segmentName ) )               
+                    # print( segmentName, 'has static rotation( helical )', client.GetSegmentStaticRotationHelical( subjectName, segmentName ) )
+                    # print( segmentName, 'has static rotation( EulerXYZ )', client.GetSegmentStaticRotationEulerXYZ( subjectName, segmentName ) )
+                    # print( segmentName, 'has static rotation( Quaternion )', client.GetSegmentStaticRotationQuaternion( subjectName, segmentName ) )
                     # print( segmentName, 'has static rotation( Matrix )', client.GetSegmentStaticRotationMatrix( subjectName, segmentName ) )
                     # try:
-                        # print( segmentName, 'has static scale', client.GetSegmentStaticScale( subjectName, segmentName ) )
+                    # print( segmentName, 'has static scale', client.GetSegmentStaticScale( subjectName, segmentName ) )
                     # except ViconDataStream.DataStreamException as e:
-                        # print( 'Scale Error', e )               
+                    # print( 'Scale Error', e )
                     # print( segmentName, 'has global translation', client.GetSegmentGlobalTranslation( subjectName, segmentName ) )
-                    # print( segmentName, 'has global rotation( helical )', client.GetSegmentGlobalRotationHelical( subjectName, segmentName ) )               
-                    # print( segmentName, 'has global rotation( EulerXYZ )', client.GetSegmentGlobalRotationEulerXYZ( subjectName, segmentName ) )               
-                    # print( segmentName, 'has global rotation( Quaternion )', client.GetSegmentGlobalRotationQuaternion( subjectName, segmentName ) )               
+                    # print( segmentName, 'has global rotation( helical )', client.GetSegmentGlobalRotationHelical( subjectName, segmentName ) )
+                    # print( segmentName, 'has global rotation( EulerXYZ )', client.GetSegmentGlobalRotationEulerXYZ( subjectName, segmentName ) )
+                    # print( segmentName, 'has global rotation( Quaternion )', client.GetSegmentGlobalRotationQuaternion( subjectName, segmentName ) )
                     # print( segmentName, 'has global rotation( Matrix )', client.GetSegmentGlobalRotationMatrix( subjectName, segmentName ) )
                     # print( segmentName, 'has local translation', client.GetSegmentLocalTranslation( subjectName, segmentName ) )
-                    # print( segmentName, 'has local rotation( helical )', client.GetSegmentLocalRotationHelical( subjectName, segmentName ) )               
-                    # print( segmentName, 'has local rotation( EulerXYZ )', client.GetSegmentLocalRotationEulerXYZ( subjectName, segmentName ) )               
-                    # print( segmentName, 'has local rotation( Quaternion )', client.GetSegmentLocalRotationQuaternion( subjectName, segmentName ) )               
+                    # print( segmentName, 'has local rotation( helical )', client.GetSegmentLocalRotationHelical( subjectName, segmentName ) )
+                    # print( segmentName, 'has local rotation( EulerXYZ )', client.GetSegmentLocalRotationEulerXYZ( subjectName, segmentName ) )
+                    # print( segmentName, 'has local rotation( Quaternion )', client.GetSegmentLocalRotationQuaternion( subjectName, segmentName ) )
                     # print( segmentName, 'has local rotation( Matrix )', client.GetSegmentLocalRotationMatrix( subjectName, segmentName ) )
-                    j[sbj_idx_str] =  {}
-                    j[sbj_idx_str]['name']=subjectName
-                    j[sbj_idx_str]['global_translation'] = {}
-                    j[sbj_idx_str]['global_translation'] = client.GetSegmentGlobalTranslation( subjectName, segmentName )
+                    j[sbj_idx_str] = {}
+                    j[sbj_idx_str]["name"] = subjectName
+                    j[sbj_idx_str]["global_translation"] = {}
+                    j[sbj_idx_str][
+                        "global_translation"
+                    ] = client.GetSegmentGlobalTranslation(subjectName, segmentName)
                     j[sbj_idx_str]["global_rotation"] = {}
-                    j[sbj_idx_str]["global_rotation"]['helical'] = client.GetSegmentGlobalRotationHelical( subjectName, segmentName )
-                    j[sbj_idx_str]["global_rotation"]['eulerxyz'] = client.GetSegmentGlobalRotationEulerXYZ( subjectName, segmentName )
-                    j[sbj_idx_str]["global_rotation"]['quaternion'] = client.GetSegmentGlobalRotationQuaternion( subjectName, segmentName )
-                    j[sbj_idx_str]["global_rotation"]['matrix'] = client.GetSegmentGlobalRotationMatrix( subjectName, segmentName )
+                    j[sbj_idx_str]["global_rotation"][
+                        "helical"
+                    ] = client.GetSegmentGlobalRotationHelical(subjectName, segmentName)
+                    j[sbj_idx_str]["global_rotation"][
+                        "eulerxyz"
+                    ] = client.GetSegmentGlobalRotationEulerXYZ(
+                        subjectName, segmentName
+                    )
+                    j[sbj_idx_str]["global_rotation"][
+                        "quaternion"
+                    ] = client.GetSegmentGlobalRotationQuaternion(
+                        subjectName, segmentName
+                    )
+                    j[sbj_idx_str]["global_rotation"][
+                        "matrix"
+                    ] = client.GetSegmentGlobalRotationMatrix(subjectName, segmentName)
                 try:
                     # print( 'Object Quality', client.GetObjectQuality( subjectName ) )
-                    j[sbj_idx_str]["quality"] = client.GetObjectQuality( subjectName )
+                    j[sbj_idx_str]["quality"] = client.GetObjectQuality(subjectName)
                 except ViconDataStream.DataStreamException as e:
-                        # print( 'Not present', e )
-                        j[sbj_idx_str]["quality"] = 'Not present'
-            
+                    # print( 'Not present', e )
+                    j[sbj_idx_str]["quality"] = "Not present"
+
             # broadcast json object
             socket.send_json(j)
 
             # print(json.dumps(j, indent=4, sort_keys=True))
-                # markerNames = client.GetMarkerNames( subjectName )
-                # for markerName, parentSegment in markerNames:
-                #     print( markerName, 'has parent segment', parentSegment, 'position', client.GetMarkerGlobalTranslation( subjectName, markerName ) )
-                #     rayAssignments = client.GetMarkerRayAssignments( subjectName, markerName )
-                #     if len( rayAssignments ) == 0:
-                #         print( 'No ray assignments for', markerName )
-                #     else:
-                #         for cameraId, centroidIndex in rayAssignments:
-                #             print( 'Ray from', cameraId, 'centroid', centroidIndex )
+            # markerNames = client.GetMarkerNames( subjectName )
+            # for markerName, parentSegment in markerNames:
+            #     print( markerName, 'has parent segment', parentSegment, 'position', client.GetMarkerGlobalTranslation( subjectName, markerName ) )
+            #     rayAssignments = client.GetMarkerRayAssignments( subjectName, markerName )
+            #     if len( rayAssignments ) == 0:
+            #         print( 'No ray assignments for', markerName )
+            #     else:
+            #         for cameraId, centroidIndex in rayAssignments:
+            #             print( 'Ray from', cameraId, 'centroid', centroidIndex )
 
             # unlabeledMarkers = client.GetUnlabeledMarkers()
             # for markerPos, trajID in unlabeledMarkers:
@@ -261,7 +305,6 @@ while tried<10:
             #     for centroid, radius, weight in centroids:
             #         print( centroid, radius, weight )
 
-
     except ViconDataStream.DataStreamException as e:
-        print( 'Handled data stream error', e )
+        print("Handled data stream error", e)
         tried = tried + 1
