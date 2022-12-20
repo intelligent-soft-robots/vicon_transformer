7fba9bbd98bfad5b53d00e797d79f48939e7a862
Felix Kloss <felix.widmaier@tuebingen.mpg.de>
2022-12-20 08:30:58 +0100

    test: Move utils to separate header file
    So they can be used in other tests as well.

--- tests/test_transform.cpp
+++ tests/test_transform.cpp
@@ -8,36 +8,9 @@
 
 #include <vicon_transformer/transform.hpp>
 
+#include "utils.hpp"
+
 using vicon_transformer::Transformation;
-
-// helper for ASSERT_PRED2
-bool matrix_almost_equal(const Eigen::MatrixXd &lhs, const Eigen::MatrixXd &rhs)
-{
-    return lhs.isApprox(rhs, 1e-8);
-}
-
-// for more convenient use
-#define ASSERT_MATRIX_ALMOST_EQUAL(m1, m2) \
-    ASSERT_PRED2(matrix_almost_equal, m1, m2)
-
-// PrintTo functions are needed such that gtest prints the actual values of the
-// vectors/matrices instead of a hex dump
-namespace Eigen
-{
-void PrintTo(const Vector3d &m, std::ostream *os)
-{
-    *os << std::endl << m.transpose() << std::endl;
-}
-
-void PrintTo(const Matrix4d &m, std::ostream *os)
-{
-    *os << std::endl << m << std::endl;
-}
-}  // namespace Eigen
-
-//
-// ---- Actual Tests ----
-//
 
 TEST(TestTransformation, identity)
 {
