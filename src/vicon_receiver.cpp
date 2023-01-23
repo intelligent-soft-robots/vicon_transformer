bb3c63887796feaca00d63f576a6908c15a04008
Felix Kloss <felix.widmaier@tuebingen.mpg.de>
2023-01-23 15:45:28 +0100

    Move ostream operator implemenation to types.cpp

--- src/vicon_receiver.cpp
+++ src/vicon_receiver.cpp
@@ -21,34 +21,6 @@ using Result = ViconDataStreamSDK::CPP::Result::Enum;
 
 namespace vicon_transformer
 {
-// FIXME: this is not the right place for the operator<< overloads
-std::ostream& operator<<(std::ostream& os, const ViconFrame& vf)
-{
-    fmt::print(os, "Frame Number: {}\n", vf.frame_number);
-    fmt::print(os, "Frame Rate: {}\n", vf.frame_rate);
-    fmt::print(os, "Latency: {}\n", vf.latency);
-    fmt::print(os, "Timestamp: {}\n", vf.time_stamp);
-
-    fmt::print(os, "Subjects ({}):\n", vf.subjects.size());
-    for (auto const& [name, data] : vf.subjects)
-    {
-        fmt::print(os, "  {}\n", name);
-        fmt::print(os, "    Visible: {}\n", data.is_visible);
-        fmt::print(os,
-                   "    Translation: {}\n",
-                   data.global_pose.translation.transpose());
-        fmt::print(os,
-                   "    Rotation: ({}, {}, {}, {})\n",
-                   data.global_pose.rotation.x(),
-                   data.global_pose.rotation.y(),
-                   data.global_pose.rotation.z(),
-                   data.global_pose.rotation.w());
-        fmt::print(os, "    Quality: {}\n", data.quality);
-    }
-
-    return os;
-}
-
 ViconReceiver::ViconReceiver(const std::string& host_name,
                              const ViconReceiverConfig& config,
                              std::shared_ptr<spdlog::logger> logger)
