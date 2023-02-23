3a5701eaa100ba87865ac07c01d86c72e391a475
Felix Kloss <felix.widmaier@tuebingen.mpg.de>
2023-02-23 11:56:22 +0100

    fix: Compute extrinsic Euler angles, not intrinsic
    It seems Eigen's `eulerAngles` function gives intrinsic angles.  Since
    in general the documentation is not super clear about what convention
    they are following, better compute it manually, based on equations from
    Wikipedia.
    
    This makes the (previously failing) unit test pass.

--- include/vicon_transformer/transform.hpp
+++ include/vicon_transformer/transform.hpp
@@ -70,7 +70,14 @@ public:
     }
 };
 
-//! Represents a 3d transformation as translation vector and Euler angles.
+/**
+ * @brief Represents a 3d transformation as translation vector and Euler angles.
+ *
+ * The convention used for the Euler angles is extrinsic xyz.  Note that these
+ * are actually not proper Euler angles but Tait-Bryan (or Cardan) angles.
+ * However, the name "Euler" is often used for them as well and probably better
+ * known, so we'll stick with this name here.
+ */
 struct EulerTransform
 {
     //! Translational part of the transform.
@@ -89,7 +96,34 @@ struct EulerTransform
     EulerTransform(const Eigen::Isometry3d &tf)
     {
         translation = tf.translation();
-        euler_xyz = tf.rotation().eulerAngles(0, 1, 2);
+
+        // Computation of the correct Tait-Bryan angles is a bit arcane...
+        //
+        // Wikipedia [1] lists equations to compute different orders of
+        // Tait-Bryan angles from a rotation matrix. However, there is no
+        // mention on Wikipedia if this is for intrinsic or extrinsic angles.
+        // Per trial and error I figured out that it is intrinsic.  We need
+        // extrinsic, though.
+        //
+        // According to [2], extrinsic xyz angles correspond to intrinsic zyx
+        // angles in reversed order. So using the equations from Wikipedia for
+        // ZYX and reversing the order of alpha, beta, gamma, we get
+        //
+        // a = atan(R21/R11)
+        // b = asin(-R31)
+        // c = atan(R32/R33)
+        //
+        // which are the desired extrinsic xyz angles.
+        //
+        // [1] en.wikipedia.org/wiki/Euler_angles#Rotation_matrix, 2023-02-23
+        // [2]
+        // en.wikipedia.org/wiki/Davenport_chained_rotations#Conversion_between_intrinsic_and_extrinsic_rotations,
+        // 2023-02-23
+
+        const Eigen::Matrix3d &R = tf.rotation();
+        euler_xyz[0] = std::atan2(R(2, 1), R(2, 2));
+        euler_xyz[1] = std::asin(-R(2, 0));
+        euler_xyz[2] = std::atan2(R(1, 0), R(0, 0));
     }
 };
 
