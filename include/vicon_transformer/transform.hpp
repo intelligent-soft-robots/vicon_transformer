2744423fe5e667d5a52d21a594c4590f9cbb9ba0
Felix Kloss <felix.widmaier@tuebingen.mpg.de>
2023-02-28 15:10:37 +0100

    Move EulerTransform implementations to cpp

--- include/vicon_transformer/transform.hpp
+++ include/vicon_transformer/transform.hpp
@@ -86,45 +86,10 @@ struct EulerTransform
     Eigen::Vector3d euler_xyz;
 
     //! Construct identity transformation.
-    EulerTransform()
-    {
-        translation.setZero();
-        euler_xyz.setZero();
-    }
+    EulerTransform();
 
-    //! Construct from a isometry transformation.
-    EulerTransform(const Eigen::Isometry3d &tf)
-    {
-        translation = tf.translation();
-
-        // Computation of the correct Tait-Bryan angles is a bit arcane...
-        //
-        // Wikipedia [1] lists equations to compute different orders of
-        // Tait-Bryan angles from a rotation matrix. However, there is no
-        // mention on Wikipedia if this is for intrinsic or extrinsic angles.
-        // Per trial and error I figured out that it is intrinsic.  We need
-        // extrinsic, though.
-        //
-        // According to [2], extrinsic xyz angles correspond to intrinsic zyx
-        // angles in reversed order. So using the equations from Wikipedia for
-        // ZYX and reversing the order of alpha, beta, gamma, we get
-        //
-        // a = atan(R21/R11)
-        // b = asin(-R31)
-        // c = atan(R32/R33)
-        //
-        // which are the desired extrinsic xyz angles.
-        //
-        // [1] en.wikipedia.org/wiki/Euler_angles#Rotation_matrix, 2023-02-23
-        // [2]
-        // en.wikipedia.org/wiki/Davenport_chained_rotations#Conversion_between_intrinsic_and_extrinsic_rotations,
-        // 2023-02-23
-
-        const Eigen::Matrix3d &R = tf.rotation();
-        euler_xyz[0] = std::atan2(R(2, 1), R(2, 2));
-        euler_xyz[1] = std::asin(-R(2, 0));
-        euler_xyz[2] = std::atan2(R(1, 0), R(0, 0));
-    }
+    //! Construct from an isometry transformation.
+    EulerTransform(const Eigen::Isometry3d &tf);
 };
 
 }  // namespace vicon_transformer
